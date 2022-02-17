c73733cffd1a62de1af35089354efbe9fa5854c0
tejal-1303 <79006362+tejal-1303@users.noreply.github.com>
2022-02-17 22:04:40 +0530

    The first commit

---
+++ src/pages/NewMeetup.js
@@ -0,0 +1,13 @@
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
+function NewMeetupPage() {
+  function addMeetupHandler(meetupData) {}
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </section>
+  );
+}
+
+export default NewMeetupPage;
